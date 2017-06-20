cefcf4c0da486adeee060f83bef743a21f727267
Justin Yoo <justin.yoo@aliencube.com>
2017-06-20 15:22:01 +1000

    Add scaffolds

---
+++ src/Yarm.Functions/IFunction.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Yarm.Functions
+{
+    /// <summary>
+    /// This provides interfaces to the <see cref="FunctionBase"/> class.
+    /// </summary>
+    public interface IFunction : IDisposable
+    {
+    }
+}
