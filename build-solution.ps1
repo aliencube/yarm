642cf740bb026c28964125f2d3ade027f34186c0
Justin Yoo <justin.yoo@aliencube.com>
2017-06-25 22:15:06 +1000

    Update build-solution.ps1

--- build-solution.ps1
+++ build-solution.ps1
@@ -14,11 +14,11 @@ Write-Host "NuGet packages restored" -ForegroundColor Green
 # Builds solution
 Write-Host "Building YARM.sln ..." -ForegroundColor Green
 
-$publishUrl = $("bin\" + $Configuration + "\PublishOutput")
-
 msbuild .\YARM.sln /t:Rebuild /p:Configuration=$Configuration /p:Platform=$Platform
 
 # Builds Function App
-msbuild .\src\Yarm.FunctionApp\Yarm.FunctionApp.csproj /t:Rebuild /p:Configuration=$Configuration /p:Platform=$Platform /p:DeployOnBuild=true /p:PublishProfile=FolderProfile.pubxml /p:LastUsedBuildConfiguration=$Configuration /p:PublishUrl=$publishUrl
+$publishUrl = $("bin\" + $Configuration + "\PublishOutput")
+
+msbuild .\src\Yarm.FunctionApp\Yarm.FunctionApp.csproj /t:Rebuild /p:Configuration=$Configuration /p:Platform=$Platform /p:OutputPath=bin /p:DeployOnBuild=true /p:PublishProfile=FolderProfile.pubxml /p:LastUsedBuildConfiguration=$Configuration /p:PublishUrl=$publishUrl
 
 Write-Host "YARM.sln built" -ForegroundColor Green
