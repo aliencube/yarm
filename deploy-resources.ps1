36681b075094637fc941f1a251d0d1685959de63
Justin Yoo <justin.yoo@aliencube.com>
2017-06-25 20:23:41 +1000

    Update deploy-resources.ps1

--- deploy-resources.ps1
+++ deploy-resources.ps1
@@ -16,30 +16,32 @@ Write-Host "Logging into Azure ..." -ForegroundColor Green
 $password = ConvertTo-SecureString $ClientSecret -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($ClientId, $password)
 
-Login-AzureRmAccount `
-    -ServicePrincipal `
-    -Tenant $TenantId `
-    -SubscriptionId $SubscriptionId `
-    -Credential $credential
+$logged = Login-AzureRmAccount `
+              -ServicePrincipal `
+              -Tenant $TenantId `
+              -SubscriptionId $SubscriptionId `
+              -Credential $credential
 
 Write-Host "Azure logged in" -ForegroundColor Green
 
 # Deploy Azure resources
 Write-Host "Deploying Azure resources ..." -ForegroundColor Green
 
-New-AzureRmResourceGroupDeployment `
-    -Name AppDeployment `
-    -ResourceGroupName $("yarm-rg-" + $Environment + "-" + $Location) `
-    -TemplateFile .\src\Yarm.ResourceGroup\app-deploy.json `
-    -TemplateParameterFile .\src\Yarm.ResourceGroup\app-deploy.parameters.json `
-    -environment $Environment `
-    -location $Location `
-    -logStorageContainerSasToken $LogStorageContainerSasToken `
-    -remoteDebuggingEnabled $true `
-    -functionAppKey $FunctionAppKey `
-    -applicationInsightsEnabled $true
+$deployed = New-AzureRmResourceGroupDeployment `
+                -Name AppDeployment `
+                -ResourceGroupName $("yarm-rg-" + $Environment + "-" + $Location) `
+                -TemplateFile .\src\Yarm.ResourceGroup\app-deploy.json `
+                -TemplateParameterFile .\src\Yarm.ResourceGroup\app-deploy.parameters.json `
+                -environment $Environment `
+                -location $Location `
+                -logStorageContainerSasToken $LogStorageContainerSasToken `
+                -remoteDebuggingEnabled $true `
+                -functionAppKey $FunctionAppKey `
+                -applicationInsightsEnabled $true
 
 Write-Host "Azure resources deployed" -ForegroundColor Green
 
 Remove-Variable password
 Remove-Variable credential
+Remove-Variable logged
+Remove-Variable deployed
